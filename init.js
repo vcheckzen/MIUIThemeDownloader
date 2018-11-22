d011af3e03ad2f3d050029e3a8e63b31501caa63
LOGI <vcheckzen@outlook.com>
2018-11-22 15:48:23 +0800

    添加更严格的 URL 判断

--- init.js
+++ init.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              小米主题下载
 // @namespace         https://github.com/vcheckzen/xiaomiThemeDownloader
-// @version           1.0.0
+// @version           1.0.1
 // @icon              http://www.xiaomi.com/favicon.ico
 // @description       在小米主题官网添加下载按钮
 // @author            vcheckzen <https://github.com/vcheckzen/xiaomiThemeDownloader/issues>
@@ -14,9 +14,12 @@
 
 (function () {
     'use strict';
+    var O = window.location.href;
+    var P = /http[s]{0,1}:\/\/zhuti.xiaomi.com\/detail\/[a-z,0-9]{8}-[a-z,0-9]{4}-[a-z,0-9]{4}-[a-z,0-9]{4}-?[a-z,0-9]{12}$/;
+    if (!P.test(O)) return;
     var $ = $ || window.$;
-    var E = ''; var M = ''; var U = window.atob("aHR0cDovL3ZlcndlbGt0ZS5haXdhci50dzo1NTU1L21pdWkvdGhlbWU="); var O = window.location.href;
-    $.ajax({ url: U, type: "post", data:{ "originUrl": O }, async: false, timeout: 2000, success: function (data) { E = data.downloadUrl; M = data.msg; } })
+    var E = ''; var M = ''; var U = window.atob("aHR0cDovL3ZlcndlbGt0ZS5haXdhci50dzo1NTU1L21pdWkvdGhlbWU=");
+    $.ajax({ url: U, type: "post", data: { "originUrl": O }, async: false, timeout: 2000, success: function (data) { E = data.downloadUrl; M = data.msg; } })
     if (M == "success") {
         var btn = "<button class=\"btn-buy J_Push\" type=\"submit\">下载</button>";
         var form = "<form class=\"detail-buy\" action=\"" + E + "\">" + btn + "</form>";
