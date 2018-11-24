66b26b8226a48deb9949b0d85a3210428efdaf5a
LOGI <vcheckzen@outlook.com>
2018-11-24 19:36:39 +0800

    UPDATE EXEC LOGIC

--- mitheme.user.js
+++ mitheme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              小米主题下载
 // @namespace         https://github.com/vcheckzen/xiaomiThemeDownloader
-// @version           1.0.4
+// @version           1.0.5
 // @icon              http://www.xiaomi.com/favicon.ico
 // @description       在小米主题官网添加下载按钮
 // @author            vcheckzen <https://github.com/vcheckzen/xiaomiThemeDownloader/issues>
@@ -16,15 +16,16 @@
     'use strict';
     var O = window.location.href;
     var P = new RegExp("^https?://zhuti\.xiaomi\.com/detail/[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}$");
-    if (!P.test(O)) return;
-    var $ = $ || window.$;
-    $.ajax({
-        url: window.atob("aHR0cDovL3ZlcndlbGt0ZS5haXdhci50dzo1NTU1L21pdWkvdGhlbWU="), type: "post", data: { "originUrl": O }, timeout: 3000, success: function (data) {
-            if (data.msg == "success") {
-                var btn = "<button class=\"btn-buy J_Push\" type=\"submit\">下载</button>";
-                var form = "<form class=\"detail-buy\" action=\"" + data.downloadUrl + "\">" + btn + "</form>";
-                $("#J-downWrap").append(form);
+    if (P.test(O)) {
+        var $ = $ || window.$;
+        $.ajax({
+            url: window.atob("aHR0cDovL3ZlcndlbGt0ZS5haXdhci50dzo1NTU1L21pdWkvdGhlbWU="), type: "post", data: { "originUrl": O }, timeout: 5000, success: function (data) {
+                if (data.msg == "success") {
+                    var btn = "<button class=\"btn-buy J_Push\" type=\"submit\">下载</button>";
+                    var form = "<form class=\"detail-buy\" action=\"" + data.downloadUrl + "\">" + btn + "</form>";
+                    $("#J-downWrap").append(form);
+                }
             }
-        }
-    })
+        });
+    }
 })();
