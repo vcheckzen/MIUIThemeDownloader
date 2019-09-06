93b9782afa20a8d5b67b23a723a76abdd98ac5ec
LOGI <vcheckzen@outlook.com>
2019-09-06 11:46:40 +0800

    chore(all): upgrade code to es6

--- miuithemedownloader.user.js
+++ miuithemedownloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              MIUI 主题下载器
 // @namespace         https://github.com/vcheckzen/MIUIThemeDownloader
-// @version           1.0.6
+// @version           1.0.7
 // @icon              http://www.xiaomi.com/favicon.ico
 // @description       恢复 MIUI 主题官网的下载按钮
 // @author            https://github.com/vcheckzen
@@ -12,23 +12,22 @@
 // @run-at            document-end
 // ==/UserScript==
 
-(function () {
+(() => {
     'use strict';
-    var O = window.location.href;
-    var R = new RegExp("^https?://zhuti\.xiaomi\.com/detail/[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}$");
+    let O = location.href;
+    const R = new RegExp('^https?://zhuti.xiaomi.com/detail/[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}$');
     if (R.test(O)) {
-        var F = window.atob("emh1dGkueGlhb21pLmNvbS9kZXRhaWw=");
-        var L = window.atob("dGhtLm1hcmtldC54aWFvbWkuY29tL3RobS9kb3dubG9hZC92Mg==");
+        const F = window.atob("emh1dGkueGlhb21pLmNvbS9kZXRhaWw=");
+        const L = window.atob("dGhtLm1hcmtldC54aWFvbWkuY29tL3RobS9kb3dubG9hZC92Mg==");
         GM_xmlhttpRequest({
             method: "GET",
             url: O.replace(F, L),
-            onload: function (res) {
-                if (res.status == 200) {
-                    var data = JSON.parse(res.responseText);
-                    if (data.apiCode == 0) {
-                        console.log(data)
-                        var btn = "<button class=\"btn-buy J_Push\" type=\"submit\">下载</button>";
-                        var form = "<form class=\"detail-buy\" action=\"" + data.apiData.downloadUrl + "\">" + btn + "</form>";
+            onload: res => {
+                if (res.status === 200) {
+                    let data = JSON.parse(res.responseText);
+                    if (data.apiCode === 0) {
+                        let btn = '<button class="btn-buy J_Push" type="submit">下载</button>';
+                        let form = '<form class="detail-buy" action="' + data.apiData.downloadUrl + '">' + btn + '</form>';
                         $("#J-downWrap").append(form);
                     }
                 }
